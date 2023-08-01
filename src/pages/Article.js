a1d7f991f438e77056439f417b79503433cdda80
WYZ00 <thanthtettin@gmail.com>
2023-08-02 00:22:32 +0630

    practice done

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import articles from "../components/article-content";
 import Articles from "../components/Articles";
 import NoteFound from "./NotFound";
+import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
 
 const Article = () => {
   const { name } = useParams();
   const article = articles.find((article) => article.name === name);
+  const [articleInfo, setArticleInfo] = useState({ comments: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
+
   if (!article) return <NoteFound />;
   const otherArticles = articles.filter((article) => article.name !== name);
   return (
@@ -17,6 +31,8 @@ const Article = () => {
           {paragraph}
         </p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
+      <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
       <h1 className="sm:text-2xl text-xl font-bold my-4 text-gray-900">
         Other Articles
       </h1>
